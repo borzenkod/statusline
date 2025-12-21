d0ce2f22a0635fc885755145b5248a725f9f3869
Dmytro Borzenko <borzenko@duck.com>
2025-12-21 15:00:35 +0000

    REFACTOR

--- build.rexx
+++ build.rexx
@@ -1,7 +1,9 @@
 
-files = "src/main.cbl src/output.cbl src/date.cbl src/date_hook.cbl src/time.cbl src/time_hook.cbl"
-files = files "src/load.cbl src/load_hook.cbl"
-files = files "src/mem.cbl src/mem_hook.cbl"
+files = "src/main.cbl src/output.cbl"
+files = files "src/time_hook.cbl"
+files = files "src/date_hook.cbl"
+files = files "src/load_hook.cbl"
+files = files "src/mem_hook.cbl"
 
 say "Compiling COBOL files: " files
 "cobc" "-x" files
--- src/main.cbl
+++ src/main.cbl
@@ -2,6 +2,8 @@
        PROGRAM-ID. MAIN.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+       01 WS-CALLBACK PROCEDURE-POINTER.
+       01 WS-OUTPUT-TYPE PIC IS 9 VALUE IS 0.
        PROCEDURE DIVISION.
        Main.
            DISPLAY "{ " QUOTE "version" QUOTE ": 1 }"
@@ -13,10 +15,26 @@
 
        LoopInner.
            DISPLAY "["
-           CALL 'DMEM'
-           CALL 'DLOAD'
-           CALL 'DDATE'
-           CALL 'DTIME'
+           SET WS-CALLBACK TO ENTRY 'DMEMHOOK'.
+           CALL
+               'OUTPUT_FMT' USING
+               WS-OUTPUT-TYPE WS-CALLBACK "ffffff"
+           END-CALL
+           SET WS-CALLBACK TO ENTRY 'DLOADHOOK'.
+           CALL
+               'OUTPUT_FMT' USING
+               WS-OUTPUT-TYPE WS-CALLBACK "ff0fff"
+           END-CALL
+           SET WS-CALLBACK TO ENTRY 'DDATEHOOK'.
+           CALL
+               'OUTPUT_FMT' USING
+               WS-OUTPUT-TYPE WS-CALLBACK "ffffff"
+           END-CALL
+           SET WS-CALLBACK TO ENTRY 'DTIMEHOOK'.
+           CALL
+               'OUTPUT_FMT' USING
+               WS-OUTPUT-TYPE WS-CALLBACK "ffff0f"
+           END-CALL
            DISPLAY "],"
 
            CONTINUE AFTER 1 SECONDS
