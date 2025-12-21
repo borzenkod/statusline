616ffedc8d1758d1e87b164b0094d77cd72ae2be
Dmytro Borzenko <borzenko@duck.com>
2025-12-21 12:26:26 +0000

    FIX: SAVE 2 BYTES

--- src/date_hook.cbl
+++ src/date_hook.cbl
@@ -4,7 +4,8 @@
        WORKING-STORAGE SECTION.
        01  WS-CURRENT-DATE-FIELDS.
         05  WS-CURRENT-DATE.
-           10  WS-CURRENT-YEAR    PIC  9(4).
+           10  FILLER             PIC  9(2).
+           10  WS-CURRENT-YEAR    PIC  9(2).
            10  WS-CURRENT-MONTH   PIC  9(2).
            10  WS-CURRENT-DAY     PIC  9(2).
         05  WS-CURRENT-TIME.
