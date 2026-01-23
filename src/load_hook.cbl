8e988419862eea911f3556d180d3db1d779fb048
Dmytro Borzenko <borzenko@duck.com>
2026-01-23 20:59:48 +0100

    CHORE(TESTS): ADD TESTS FOR 'LOAD' MODULE

--- src/load_hook.cbl
+++ src/load_hook.cbl
@@ -7,12 +7,19 @@
            05 LOAD5  USAGE COMP-2.
            05 LOAD15 USAGE COMP-2.
        01 DIS PIC IS 99.99.
-       PROCEDURE DIVISION.
+       LINKAGE SECTION.
+       01 L-BODY                  PIC X(42).
+       PROCEDURE DIVISION USING L-BODY.
            CALL
                'getloadavg' USING
                BY REFERENCE LOAD BY VALUE 3
            END-CALL
-           MOVE LOAD1                  TO DIS.
+           EVALUATE L-BODY
+               WHEN "1M"  MOVE LOAD1    TO DIS
+               WHEN "5M"  MOVE LOAD5    TO DIS
+               WHEN "15M" MOVE LOAD15   TO DIS
+               WHEN OTHER MOVE 0        TO DIS
+           END-EVALUATE
            DISPLAY DIS WITH NO ADVANCING
            END-DISPLAY
            GOBACK.
